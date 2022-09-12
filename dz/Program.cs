5c79289ab9cde1ce2c914babddfbb291a181d7a8
Roman <romangushchintlt@gmail.com>
2022-09-12 20:20:15 +0400

    выполнил задачку 43

--- dz/Program.cs
+++ dz/Program.cs
@@ -3,52 +3,72 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 4
 
-Console.Write("Введите числа: ");
-int[] num = StringToNum(Console.ReadLine());
+// Console.Write("Введите числа: ");
+// int[] num = StringToNum(Console.ReadLine());
 
-int[] StringToNum(string input)
-{
-    int count = 1;
-    for (int i = 0; i < input.Length; i++)
-    {
-        if (input[i] == ' ')
-        {
-            count++;
-        }
-    }
+// int[] StringToNum(string input)
+// {
+//     int count = 1;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ' ')
+//         {
+//             count++;
+//         }
+//     }
 
-    int[] num = new int [count];
-    int index = 0;
+//     int[] num = new int [count];
+//     int index = 0;
 
-    for (int i = 0; i < input.Length; i++)
-    {
-        string temp = "";
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         string temp = "";
 
-        while (input [i] != ' ')
-        {
-        if(i != input.Length - 1)
-        {
-            temp += input [i].ToString();
-            i++;
-        }
-        else
-        {
-            temp += input [i].ToString();
-            break;
-        }
-        }
-        num[index] = Convert.ToInt32(temp);
-        index++;
-    }
-    return num;
-}
+//         while (input [i] != ' ')
+//         {
+//         if(i != input.Length - 1)
+//         {
+//             temp += input [i].ToString();
+//             i++;
+//         }
+//         else
+//         {
+//             temp += input [i].ToString();
+//             break;
+//         }
+//         }
+//         num[index] = Convert.ToInt32(temp);
+//         index++;
+//     }
+//     return num;
+// }
 
-int sum = 0;
-for (int i = 0; i < num.Length; i++)
-{
-    if (num[i] > 0)
-    {
-        sum++;
-    }
-}
-Console.WriteLine($"Количество чисел больше 0 = {sum}");
+// int sum = 0;
+// for (int i = 0; i < num.Length; i++)
+// {
+//     if (num[i] > 0)
+//     {
+//         sum++;
+//     }
+// }
+// Console.WriteLine($"Количество чисел больше 0 = {sum}");
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//Например:
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+
+double x = (b2 - b1) / (k1 - k2);
+double y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+
+Console.WriteLine($"точка пересечения двух прямых -> ({x}; {y})");
